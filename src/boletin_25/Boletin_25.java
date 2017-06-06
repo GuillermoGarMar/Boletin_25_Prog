cec74dc25e348fbcdf6d2faf72d7040fb5c243a1
guille <guille@guille-Lenovo-Z50-70>
2017-06-07 00:41:39 +0200

    v1

---
+++ src/boletin_25/Boletin_25.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_25;
+
+/**
+ *
+ * @author guille
+ */
+public class Boletin_25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ej1 n1 = new Ej1();
+        n1.setVisible(true);
+        Ej2 n2 = new Ej2();
+        n2.setVisible(true);
+    }
+    
+}
